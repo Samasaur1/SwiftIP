b0e2a5af58dc65c10c136fbd68c055a0122701a1
Sam <samgauck@icloud.com>
2018-11-09 14:07:52 -0500

    Add throwing functions that return non-optionals (closes #1)

--- Sources/SwiftIP/SwiftIP.swift
+++ Sources/SwiftIP/SwiftIP.swift
@@ -10,12 +10,22 @@ public final class IP {
         
         var ip: String? = nil
         let task = session.dataTask(with: request) { (data, response, error) in
-            ip = String(data: data!, encoding: .utf8)
+            guard let data = data else {
+                return
+            }
+            ip = String(data: data, encoding: .utf8)
             semaphore.signal()
         }
         
         task.resume()
         semaphore.wait()
+        return ip
+    }
+    
+    public class func `public`() throws -> String {
+        guard let ip = publicIP else {
+            throw Error()
+        }
         return ip
     }
     
@@ -29,16 +39,19 @@ public final class IP {
             // For each interface ...
             var ptr = ifaddr
             while ptr != nil {
-                let flags = Int32(ptr!.pointee.ifa_flags)
-                var addr = ptr?.pointee.ifa_addr.pointee
+                guard let pointer = ptr else {
+                    return nil
+                }
+                let flags = Int32(pointer.pointee.ifa_flags)
+                var addr = pointer.pointee.ifa_addr.pointee
                 
                 // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
                 if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
-                    if addr!.sa_family == UInt8(AF_INET) || addr!.sa_family == UInt8(AF_INET6) {
+                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
                         
                         // Convert interface address to a human readable string:
                         var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                        if (getnameinfo(&addr!, socklen_t(addr!.sa_len), &hostname, socklen_t(hostname.count),
+                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
                                         nil, socklen_t(0), NI_NUMERICHOST) == 0) {
                             //                        if let address = String(cString: hostname) {
                             //                            addresses.append(address)
@@ -47,11 +60,20 @@ public final class IP {
                         }
                     }
                 }
-                ptr = ptr?.pointee.ifa_next
+                ptr = pointer.pointee.ifa_next
             }
             freeifaddrs(ifaddr)
         }
         
         return addresses.first
     }
+    
+    public class func local() throws -> String {
+        guard let ip = localIP else {
+            throw Error()
+        }
+        return ip
+    }
+    
+    public struct Error: Swift.Error {}
 }
