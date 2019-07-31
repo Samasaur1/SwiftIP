774647eadb9552098dcab89c0639454982665d3d
Sam <samgauck@icloud.com>
2019-07-31 15:04:23 -0400

    Use ipconfig instead of that C monstrosity

--- Sources/SwiftIP/SwiftIP.swift
+++ Sources/SwiftIP/SwiftIP.swift
@@ -30,42 +30,67 @@ public final class IP {
     }
     
     public class var localIP: String? {
-        var addresses = [String]()
-        
-        // Get list of all interfaces on the local machine:
-        var ifaddr : UnsafeMutablePointer<ifaddrs>? = nil
-        if getifaddrs(&ifaddr) == 0 {
-            
-            // For each interface ...
-            var ptr = ifaddr
-            while ptr != nil {
-                guard let pointer = ptr else {
-                    return nil
+//        var addresses = [String]()
+//
+//        // Get list of all interfaces on the local machine:
+//        var ifaddr : UnsafeMutablePointer<ifaddrs>? = nil
+//        if getifaddrs(&ifaddr) == 0 {
+//
+//            // For each interface ...
+//            var ptr = ifaddr
+//            while ptr != nil {
+//                guard let pointer = ptr else {
+//                    return nil
+//                }
+//                let flags = Int32(pointer.pointee.ifa_flags)
+//                var addr = pointer.pointee.ifa_addr.pointee
+//
+//                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+//                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+//                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+//
+//                        // Convert interface address to a human readable string:
+//                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+//                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
+//                                        nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+//                            //                        if let address = String(cString: hostname) {
+//                            //                            addresses.append(address)
+//                            //                        }
+//                            addresses.append(String(cString: hostname))
+//                        }
+//                    }
+//                }
+//                ptr = pointer.pointee.ifa_next
+//            }
+//            freeifaddrs(ifaddr)
+//        }
+//
+//        return addresses.first
+        var output: String? = nil
+        do {
+            var en = 0
+            while output?.isEmpty ?? true {
+                let task = Process()
+                if #available(OSX 10.13, *) {
+                    task.executableURL = URL(fileURLWithPath: "/usr/sbin/ipconfig")
+                } else {
+                    task.launchPath = "/usr/sbin/ipconfig"
                 }
-                let flags = Int32(pointer.pointee.ifa_flags)
-                var addr = pointer.pointee.ifa_addr.pointee
-                
-                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
-                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
-                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
-                        
-                        // Convert interface address to a human readable string:
-                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
-                                        nil, socklen_t(0), NI_NUMERICHOST) == 0) {
-                            //                        if let address = String(cString: hostname) {
-                            //                            addresses.append(address)
-                            //                        }
-                            addresses.append(String(cString: hostname))
-                        }
-                    }
+                task.arguments = ["getifaddr", "en\(en)"]
+                let pipe = Pipe()
+                task.standardOutput = pipe
+                if #available(OSX 10.13, *) {
+                    try task.run()
+                } else {
+                    task.launch()
                 }
-                ptr = pointer.pointee.ifa_next
+                let data = pipe.fileHandleForReading.readDataToEndOfFile()
+                output = String(data: data, encoding: .utf8)
+                en += 1
             }
-            freeifaddrs(ifaddr)
-        }
-        
-        return addresses.first
+        } catch { return nil }
+
+        return output
     }
     
     public class func local() throws -> String {
