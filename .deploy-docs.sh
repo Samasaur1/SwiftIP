aed71fbf272222122f2f175e0331bb592f985630
Sam <samgauck@icloud.com>
2019-10-04 08:46:03 -0400

    Add docs, installation instructions, and basic usage

---
+++ .deploy-docs.sh
@@ -0,0 +1,20 @@
+set -o errexit
+
+gem install jazzy
+
+rm -rf docs
+rm -rf SwiftIP.xcodeproj
+swift package generate-xcodeproj
+git clone --branch=gh-pages https://github.com/Samasaur1/SwiftIP.git docs
+jazzy -o newdocs -x -target,SwiftIP
+rm -rf docs/*
+mv newdocs/* docs/
+rm -rf newdocs/
+cd docs
+echo 'section > section > p > img { margin-top: 4em; margin-right: 2em; }' >> css/jazzy.css
+git config --global user.name "Documentation Bot"
+git config --global user.email "docbot@travis-ci.com"
+git add .
+git commit -m "Update documentation"
+git remote set-url origin https://${GH_TOKEN}@github.com/Samasaur1/SwiftIP.git
+git push
