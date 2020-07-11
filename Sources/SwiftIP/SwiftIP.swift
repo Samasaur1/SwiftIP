f42abe09b9d35377092071cc64e5a11621aa0a64
Sam <samgauck@icloud.com>
2020-07-10 21:07:00 -0400

    Specify IP type; removing throwing versions

--- Sources/SwiftIP/SwiftIP.swift
+++ Sources/SwiftIP/SwiftIP.swift
@@ -1,13 +1,20 @@
 import Foundation
 
 public final class IP {
-    public class var publicIP: String? {
+    public enum Version: String {
+        case IPv4 = "ipv4"
+        case IPv6 = "ipv6"
+    }
+
+    private init() {}
+
+    public class func `public`(_ version: Version = .IPv4) -> String? {
         let semaphore = DispatchSemaphore(value: 0)
-        let url = URL(string: "https://icanhazip.com")!//i can haz ip
+        let url = URL(string: "https://\(version.rawValue).icanhazip.com")!//i can haz ip
         let request = URLRequest(url: url)
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
-        
+
         var ip: String? = nil
         let task = session.dataTask(with: request) { (data, response, error) in
             guard let data = data else {
@@ -16,26 +23,19 @@ public final class IP {
             ip = String(data: data, encoding: .utf8)
             semaphore.signal()
         }
-        
+
         task.resume()
         semaphore.wait()
         return ip
     }
-    
-    public class func `public`() throws -> String {
-        guard let ip = publicIP else {
-            throw Error()
-        }
-        return ip
-    }
-    
-    public class var localIP: String? {
+
+    public class func local(_ version: Version = .IPv4) -> String? {
         var addresses = [String]()
-        
+
         // Get list of all interfaces on the local machine:
         var ifaddr : UnsafeMutablePointer<ifaddrs>? = nil
         if getifaddrs(&ifaddr) == 0 {
-            
+
             // For each interface ...
             var ptr = ifaddr
             while ptr != nil {
@@ -44,11 +44,11 @@ public final class IP {
                 }
                 let flags = Int32(pointer.pointee.ifa_flags)
                 var addr = pointer.pointee.ifa_addr.pointee
-                
+
                 // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
                 if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
-                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
-                        
+                    if addr.sa_family == UInt8(version == .IPv4 ? AF_INET : AF_INET6) {
+
                         // Convert interface address to a human readable string:
                         var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
                         if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
@@ -64,16 +64,7 @@ public final class IP {
             }
             freeifaddrs(ifaddr)
         }
-        
+
         return addresses.first
     }
-    
-    public class func local() throws -> String {
-        guard let ip = localIP else {
-            throw Error()
-        }
-        return ip
-    }
-    
-    public struct Error: Swift.Error {}
 }
